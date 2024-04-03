aa6654b871bf20b2dd7667dbaeb6b075d56b2a71
imranrehman-it <imran.rehman@mail.utoronto.ca>
2024-04-03 19:34:04 -0400

    Add another debt label added on InputForm

--- debt-consolidation/src/components/InputForm/InputForm.js
+++ debt-consolidation/src/components/InputForm/InputForm.js
@@ -9,10 +9,10 @@ export const InputForm = () => {
       </h2>
       <div className="flex flex-col gap-2 overflow-y-auto mt-3">
         <div className="grid grid-cols-4 gap-4 ">
-        <InputLabel labelText="DEBT NAME" />
-        <InputLabel labelText="REMAINING DEBT AMOUNT" />
-        <InputLabel labelText="CURRENT APR" />
-        <InputLabel labelText="CURRENT MONTHLY PAYMENT" />
+          <InputLabel labelText="DEBT NAME" />
+          <InputLabel labelText="REMAINING DEBT AMOUNT" />
+          <InputLabel labelText="CURRENT APR" />
+          <InputLabel labelText="CURRENT MONTHLY PAYMENT" />
           <input
             type="number"
             className="p-2 mt-1 w-full bg-[#F4FBFD] border shadow-inner border-gray-300 text-black"
@@ -29,6 +29,12 @@ export const InputForm = () => {
             type="number"
             className="p-2 mt-1 w-full bg-[#F4FBFD] border shadow-inner border-gray-300 text-black"
           />
+          <button
+            className="flex items-center gap-1.5 mt-4 text-base text-semibold text-cyan-600"
+          >
+            <span className="font-bold">+</span>
+            <span>Add Another Debt</span>
+          </button>
         </div>
         <button className="w-full p-2 mt-6 text-base text-white bg-[#06A9DB] rounded">
           Calculate Savings
