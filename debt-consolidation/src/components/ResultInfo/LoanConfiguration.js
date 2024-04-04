2333b61befeceadda9ba02dd602586228d2628dc
imranrehman-it <imran.rehman@mail.utoronto.ca>
2024-04-03 21:34:44 -0400

    created component to house configuration

---
+++ debt-consolidation/src/components/ResultInfo/LoanConfiguration.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import {AprConfiguration} from './AprConfiguration'
+import {TermConfiguration} from './TermConfiguration'
+
+export const LoanConfiguration = () => {
+  return (
+    <div className="flex flex-col text-black border shadow-inner border-gray-300 p-5 gap-5">
+      <div className="flex flex-col">
+        <text className="text-[14px] font-semibold tracking-wider text-slate-900">
+          CONFIGURE YOUR CONSOLIDATED LOAN
+        </text>
+        <text className="text-[14px] font-light text-slate-900">
+          Use the slider below to simulate the new APR and loan term.
+        </text>
+      </div>
+      <AprConfiguration />
+      <TermConfiguration />
+    </div>
+  );
+}
--- debt-consolidation/src/components/ResultInfo/ResultInfo.js
+++ debt-consolidation/src/components/ResultInfo/ResultInfo.js
@@ -1,8 +1,7 @@
 import React from 'react'
-import {AprConfiguration} from './AprConfiguration'
-import {TermConfiguration} from './TermConfiguration'
 import {RepaymentSection} from './RepaymentSection'
 import {MonthlySection} from './MonthlySection'
+import {LoanConfiguration} from './LoanConfiguration'
 
 export const ResultInfo = () => {
   return (
@@ -10,18 +9,7 @@ export const ResultInfo = () => {
       <text className="text-[14px] tracking-wider text-[#06A9DB] mt-1 mb-1 p-4 font-semibold">
         Update Your Current Debts
       </text>
-      <div className="flex flex-col text-black border shadow-inner border-gray-300 p-5 gap-5">
-        <div className="flex flex-col">
-          <text className="text-[14px] font-semibold tracking-wider text-slate-900">
-            CONFIGURE YOUR CONSOLIDATED LOAN
-          </text>
-          <text className="text-[14px] font-light text-slate-900">
-            Use the slider below to simulate the new APR and loan term.
-          </text>
-        </div>
-        <AprConfiguration />
-        <TermConfiguration />
-      </div>
+      <LoanConfiguration />
       <div className="flex flex-row h-1/3">
         <RepaymentSection />
         <MonthlySection />
